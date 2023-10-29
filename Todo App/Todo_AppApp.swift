f2e3bda615cef6aad62eee81c9723c3dd3097dc0
John Kramer <hkur.gobimbel@gmail.com>
2023-10-29 18:57:01 +0700

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Mac on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
